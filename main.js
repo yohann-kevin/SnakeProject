46b86c27f1ed4862cd988a0ed6e2271e745bbbf9
Perriguey <yo44prg@icloud.com>
2020-02-06 23:30:50 +0100

    create-project

---
+++ main.js
@@ -0,0 +1,36 @@
+window.onload = function(){
+    
+    var canvas;
+    var ctx;
+    var delay = 100;
+    var xCoord = 0;
+    var yCoord = 0;
+    
+    init();
+
+    function init(){
+        
+        canvas = document.createElement('canvas');
+        canvas.width = 900;
+        canvas.height = 600;
+        canvas.style.border = "3px solid";
+        document.body.appendChild(canvas);
+        ctx = canvas.getContext('2d');
+        refreshCanvas();
+
+    }
+
+    function refreshCanvas(){
+
+        xCoord += 5;
+        yCoord += 5;
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        ctx.fillStyle = '#ff0000';
+        ctx.fillRect( xCoord , yCoord , 100 , 50);
+        setTimeout(refreshCanvas,delay);
+        
+        
+        console.log('plop');
+    }
+    
+}
