abbbd2876c5e6cfe2c6877d6f318399dcf13e291
Perriguey <yo44prg@icloud.com>
2020-02-19 16:10:37 +0100

    moveSnake

--- main.js
+++ main.js
@@ -1,61 +1,329 @@
-window.onload = function(){
-    
+// window.onload = function () {
+
+//     var canvasWidth = 900; //taille du canvas
+//     var canvasHeight = 600; //taille du canvas
+//     var blockSize = 30; //taille d'un block
+//     var ctx;
+//     var delay = 100; //delay de rafraichissement de la page
+//     var snakee;
+
+//     //appelle de fonction
+//     init();
+
+//     function init() {
+
+//         canvas = document.createElement('canvas'); //cree l'element canvas
+//         canvas.width = canvasWidth;
+//         canvas.height = canvasHeight;
+//         canvas.style.border = "3px solid";
+//         canvas.style.borderRadius = "50px";
+//         //document.body.appendChild(canvas);//fixe le canvas a l'element html body
+//         document.getElementById("snake").appendChild(canvas); //fixe le canvas a mon main
+//         ctx = canvas.getContext('2d'); //indique le contexte
+//         //creer un serpent avec 3block placé en fonction d x et y
+//         snakee = new snake([
+//             [6, 4],
+//             [5, 4],
+//             [4, 4],
+//             "right"
+//         ]);
+//         refreshCanvas(); //appel la fonction refresh
+
+//     }
+
+//     function refreshCanvas() {
+
+//         ctx.clearRect(0, 0, canvasWidth, canvasHeight); //efface le contenu
+//         //appelle la méthode advance
+//         snakee.advance();
+//         //appelle la méthode draw
+//         snakee.draw();
+//         //refresh le canvas toute les 100 miliseconde
+//         setTimeout(refreshCanvas, delay);
+
+//     }
+
+//     //déssine un block
+//     function drawBlock(ctx, position) {
+//         var x = position[0] * blockSize;
+//         var y = position[1] * blockSize;
+//         ctx.fillRect(x, y, blockSize, blockSize);
+//     }
+
+//     //fonction constructeur
+//     function snake(body, direction) {
+//         this.body = body;
+//         this.direction = direction;
+//         this.draw = function () {
+//             //sauvegarde le contenu du canvas
+//             ctx.save();
+//             ctx.fillStyle = "#ff0000";
+//             for (var i = 0; i < this.body.length; i++) {
+//                 //appelle la foction drawBlock
+//                 drawBlock(ctx, this.body[i]);
+//             }
+//             ctx.restore();
+//         };
+
+//         // this.advance= function(){
+
+//         //     var nextPosition = this.body[0].slice();
+//         //     nextPosition[0] += 1;
+//         //     this.body.unshift(nextPosition);
+//         //     this.body.pop();
+//         // };
+
+//         //permet a notre serpent de se déplacer
+//         //     this.advance = function () {
+//         //         //copie un élément de mon Array
+//         //         var nextPosition = this.body[0].slice();
+//         //         switch(this.direction) {
+//         //             case "left":
+//         //                 nextPosition[0] -= 1;
+//         //                 break;
+//         //             case "right":
+//         //                 nextPosition[0] += 1;
+//         //                 break;
+//         //             case "down":
+//         //                 nextPosition[1] += 1;
+//         //                 break
+//         //             case "up":
+//         //                 nextPosition[1] -= 1;
+//         //                 break;
+//         //             default:
+//         //                 throw("Invalid Direction");
+//         //         }
+//         //         this.body.unshift(nextPosition);
+//         //         //supprime le derniere element d'un array
+//         //         this.body.pop();
+//         //     };
+
+//         //     //réstreint les directions possible
+//         //     this.setDirection = function(newDirection) {
+//         //         var allowedDirections;
+//         //         switch(this.direction) {
+//         //             case "left":
+//         //             case "right":
+//         //                 allowedDirections = ["up", "down"];
+//         //                 break;
+//         //             case "down":
+//         //             case "up":
+//         //                 allowedDirections = ["left", "right"];
+//         //                 break;
+//         //             default:
+//         //                 throw ("Invalid Direction");
+//         //         }
+//         //         if (allowedDirections.indexOf(newDirection) > -1) {
+//         //             this.direction = newDirection;
+//         //         }
+//         //     };
+//         // }
+
+//         // //associe les touches a des directions
+//         // document.onkeydown = function handleKeyDown(e) {
+//         //     var key = e.keyCode;
+//         //     var newDirection;
+//         //     switch(key) {
+//         //         case 37:
+//         //             newDirection = "left";
+//         //             break;
+//         //         case 38:
+//         //             newDirection = "up";
+//         //             break;
+//         //         case 39:
+//         //             newDirection = "right";
+//         //             break;
+//         //         case 40:
+//         //             newDirection = "down";
+//         //             break;
+//         //         default:
+//         //             return;
+//         //     }
+//         //     snakee.setDirection(newDirection);
+//         this.advance = function () {
+//             var nextPosition = this.body[0].slice();
+//             switch (this.direction) {
+//                 case 'right':
+//                     nextPosition[0] += 1;
+//                     break;
+//                 case 'left':
+//                     nextPosition[0] -= 1;
+//                     break;
+//                 case 'down':
+//                     nextPosition[1] += 1;
+//                     break;
+//                 case 'up':
+//                     nextPosition[1] -= 1;
+//                     break;
+//                 default:
+//                     throw ("Invalid direction");
+//             }
+//             //nextPosition[0] += 1;
+//             this.body.unshift(nextPosition);
+//             this.body.pop();
+//         }
+//         this.setDirection = function (newDirection) {
+//             var allowedDirections;
+//             switch (this.direction) {
+//                 case "left":
+//                 case "right":
+//                     allowedDirections = ["up", "down"];
+//                     break;
+//                 case "down":
+//                 case "up":
+//                     allowedDirections = ["right", "left"];
+//                     break;
+//                 default:
+//                     throw ("Invalid direction");
+//             }
+//             if (allowedDirections.indexOf(newDirection) > -1) {
+//                 this.direction = newDirection;
+//             }
+//         }
+//     }
+//     document.onkeydown = function handleKeyDown(e) {
+//         var key = e.keyCode;
+//         var newDirection;
+//         switch (key) {
+//             case 37:
+//                 newDirection = "left";
+//                 break;
+//             case 38:
+//                 newDirection = "up";
+//                 break;
+//             case 39:
+//                 newDirection = "right";
+//                 break;
+//             case 40:
+//                 newDirection = "down";
+//                 break;
+//             default:
+//                 return;
+//         }
+//         //alert(newDirection);
+//         snakee.setDirection(newDirection);
+//         console.log(keyCode);
+//     }
+
+// }
+window.onload = function () {
+
+    //var canvas;
+    var ctx;
+    var delay = 100;
     var canvasWidth = 900;
     var canvasHeight = 600;
     var blockSize = 30;
-    var ctx;
-    var delay = 100;
     var snakee;
-    
+
     init();
 
-    function init(){
-        
-        canvas = document.createElement('canvas');
+    function init() {
+        var canvas = document.createElement('canvas');
         canvas.width = canvasWidth;
         canvas.height = canvasHeight;
         canvas.style.border = "3px solid";
-        document.body.appendChild(canvas);
+        // canvas.style.borderRadius = "50px";
+        // document.body.appendChild(canvas);
+        document.getElementById("snake").appendChild(canvas); //fixe le canvas a mon main
         ctx = canvas.getContext('2d');
-        snakee = new snake([[6,4],[5,4],[4,4]]);
+        snakee = new Snake([
+            [6, 4],
+            [5, 4],
+            [4, 4]
+        ], 'right');
         refreshCanvas();
-
     }
 
-    function refreshCanvas(){
+    function refreshCanvas() {
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-        ctx.clearRect(0,0,canvasWidth, canvasHeight);
-        snakee.advance();
         snakee.draw();
-        setTimeout(refreshCanvas,delay);
+        snakee.advance();
+
+        setTimeout(refreshCanvas, delay);
 
     }
 
-    function darwBlock(ctx, position){
-
+    function drawBlock(ctx, position) {
         var x = position[0] * blockSize;
         var y = position[1] * blockSize;
-        ctx.fillRect(x,y,blockSize,blockSize);
+        ctx.fillRect(x, y, blockSize, blockSize);
     }
 
-    function snake(body){
+    function Snake(body, direction) {
         this.body = body;
-        this.draw = function(){
-
+        this.direction = direction;
+        this.draw = function () {
             ctx.save();
-            ctx.fillStyle = "#ff0000";
-            for(var i = 0 ; i < this.body.length ; i++){
-                darwBlock(ctx, this.body[i]);
+            ctx.fillStyle = 'red';
+            for (var i = 0; i < this.body.length; i++) {
+                drawBlock(ctx, this.body[i]);
             }
-            ctx.restore();       
-        };
-        this.advance= function(){
-
+            ctx.restore();
+        }
+        this.advance = function () {
             var nextPosition = this.body[0].slice();
-            nextPosition[0] += 1;
+            switch (this.direction) {
+                case 'right':
+                    nextPosition[0] += 1;
+                    break;
+                case 'left':
+                    nextPosition[0] -= 1;
+                    break;
+                case 'down':
+                    nextPosition[1] += 1;
+                    break;
+                case 'up':
+                    nextPosition[1] -= 1;
+                    break;
+                default:
+                    throw ("Invalid direction");
+            }
+            //nextPosition[0] += 1;
             this.body.unshift(nextPosition);
             this.body.pop();
-        };
+        }
+        this.setDirection = function (newDirection) {
+            var allowedDirections;
+            switch (this.direction) {
+                case "left":
+                case "right":
+                    allowedDirections = ["up", "down"];
+                    break;
+                case "down":
+                case "up":
+                    allowedDirections = ["right", "left"];
+                    break;
+                default:
+                    throw ("Invalid direction");
+            }
+            if (allowedDirections.indexOf(newDirection) > -1) {
+                this.direction = newDirection;
+            }
+        }
     }
-    
+    document.onkeydown = function handleKeyDown(e) {
+        var key = e.keyCode;
+        var newDirection;
+        switch (key) {
+            case 81:
+                newDirection = "left";
+                break;
+            case 90:
+                newDirection = "up";
+                break;
+            case 68:
+                newDirection = "right";
+                break;
+            case 83:
+                newDirection = "down";
+                break;
+            default:
+                return;
+        }
+        //alert(newDirection);
+        snakee.setDirection(newDirection);
+    }
+
 }
